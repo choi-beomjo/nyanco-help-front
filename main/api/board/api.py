6c0d45c75f789f6de9ede7130ce02ce1738e4a8b
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2024-06-11 23:37:42 +0900

    feature: 태그 클래스 추가 (#1)

--- main/api/board/api.py
+++ main/api/board/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from ..tags import Tags
 
 
 router = APIRouter()
 
-@router.get("/board")
+@router.get("/board", tags=[Tags.board])
 def get_test_code():
     return {"respose": 200}
