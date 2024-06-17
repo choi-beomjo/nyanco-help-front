035672a11b76902b4a3d3a5d3a378ee84de7fbaf
최범조 <beomjo.choi@stans.co.kr>
2024-06-17 15:22:38 +0900

    feature: 공통 메시지 유틸 작성 (#4)

--- main/api/user/api.py
+++ main/api/user/api.py
@@ -4,6 +4,7 @@ from ..tags import Tags
 from db.crud.crud import get_crud, CRUD
 from .utils import *
 from .models import *
+from utils.msg.msg import Msg
 
 router = APIRouter(tags=[Tags.user])
 
@@ -20,13 +21,13 @@ def get_user(user_id: int, crud: CRUD = Depends(get_crud)):
     return User.from_orm(db_user)
 
 
-@router.post("/")
+@router.post("/", response_model=User)
 def add_user(user_info: UserInfo, crud: CRUD = Depends(get_crud)):
     db_user = add_user_to_db(user_info=user_info, crud=crud)
     return User.from_orm(db_user)
 
 
-@router.delete("/{user_id}")
+@router.delete("/{user_id}", response_model=Msg)
 def delete_user(user_id: int, crud: CRUD = Depends(get_crud)):
     delete_user_from_db(user_id=user_id, crud=crud)
-    return {"response": 200}
+    return Msg(msg="success")
