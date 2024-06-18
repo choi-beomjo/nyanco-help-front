618ae4292ae6a0ae9575b65f2a8b66eb47189bad
최범조 <beomjo.choi@stans.co.kr>
2024-06-18 15:36:32 +0900

    feature: 검증 패스워드 일치 여부 추가 (#10)

--- main/api/user/models.py
+++ main/api/user/models.py
@@ -22,16 +22,17 @@ class SignUpInfo(BaseModel):
     password1: str
     password2: str
     email: EmailStr
-'''
-    @root_validator(pre=True)
-    def not_empty(cls, v):
-        if not v or not v.strip():
-            raise ValueError('빈 값은 허용되지 않습니다.')
-        return v
 
     @root_validator(pre=True)
-    def passwords_match(cls, v, info: FieldValidationInfo):
-        if 'password1' in info.data and v != info.data['password1']:
-            raise ValueError('비밀번호가 일치하지 않습니다')
+    def not_empty(cls, values):
+        for v in values:
+            if not v or not v.strip():
+                raise ValueError('빈 값은 허용되지 않습니다.')
+        return values
+
+    @root_validator(pre=True)
+    def passwords_match(cls, values):
+        if values.get('password1') != values.get('password2'):
+            raise ValueError('비밀번호가 일치하지 않습니다.')
         return v
-        '''
+        
