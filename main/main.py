69309535c242b4736a48ce00b66a3d33762cd37e
최범조 <beomjo.choi@stans.co.kr>
2024-12-09 11:15:03 +0900

    feature: CORS 추가 (#5)

--- main/main.py
+++ main/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from api.api import api
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 보안 강화를 위해 특정 도메인으로 제한 가능
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api, prefix="/api")
 
