2102f7b4f5e923024c8908c695536c2fd9f1cdc2
최범조 <beomjo.choi@stans.co.kr>
2024-12-09 14:48:26 +0900

    refactor: 이동한 원래 함수 삭제 (#14)

--- app/api/domain/user/api.py
+++ app/api/domain/user/api.py
@@ -63,22 +63,3 @@ def user_login(form_data: OAuth2PasswordRequestForm = Depends(),
         "token_type": "bearer",
         "username": user.name
     }
-
-
-def get_current_user(token: str = Depends(oauth2_scheme),
-                     crud: CRUD = Depends(get_crud)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    else:
-        user = get_user_by_name(username, crud=crud)
-        return user
