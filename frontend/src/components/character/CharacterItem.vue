8604b006b594f615aac6c775a2564d1e4b69f7aa
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2025-02-10 00:25:23 +0900

    feature: 캐릭터 페이지 디자인 추가 (#39)

--- frontend/src/components/character/CharacterItem.vue
+++ frontend/src/components/character/CharacterItem.vue
@@ -1,29 +1,64 @@
 <template>
-    <li>
-      <strong>ID:</strong> {{ character.id }},
-      <strong>Name:</strong> {{ character.name }},
-      <strong>ATK:</strong> {{ character.atk }},
-      <strong>HP:</strong> {{ character.hp }},
-      <strong>Range:</strong> {{ character.range }},
-      <strong>Grade:</strong> {{ character.grade }},
-      <strong>Skills:</strong> {{ character.skills }},
-      <strong>Properties</strong> {{ character.properties }}
+  <li class="character-item">
+    <div class="character-info">
+      <strong>ID</strong> <span>{{ character.id }}</span>
+      <strong>Name</strong> <span>{{ character.name }}</span>
+      <strong>ATK</strong> <span>{{ character.atk }}</span>
+      <strong>HP</strong> <span>{{ character.hp }}</span>
+      <strong>Range</strong> <span>{{ character.range }}</span>
+      <strong>Grade</strong> <span>{{ character.grade }}</span>
+
+      <strong>Skills</strong>
+      <div class="skill-list">
+        <span v-for="skill in character.skills" :key="skill.id" class="skill-tag">
+          <span v-html="getSkillIcon(skill.name)"></span> 
+          {{ skill.name }}
+        </span>
+      </div>
+
+      <strong>Properties</strong>
+      <div class="property-list">
+        <span
+          v-for="property in character.properties"
+          :key="property.id"
+          :style="{ backgroundColor: getPropertyColor(property.name), color: getTextColor(property.name) }"
+          class="property-tag"
+        >
+          {{ property.name }}
+        </span>
+      </div>
+    </div>
+
+    <div class="character-actions">
       <button v-if="showEditButton" @click="$emit('edit', character.id)">Edit</button>
-    </li>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      character: {
-        type: Object,
-        required: true,
-      },
-      showEditButton: {
-        type: Boolean,
-        default: true, // 기본값은 추천 버튼을 보이게 설정
+    </div>
+  </li>
+</template>
+
+<script>
+import "@/assets/css/CharacterItem.css";
+import "@fortawesome/fontawesome-free/css/all.css"; // Font Awesome Import
+import { useIconAndColor } from "@/composables/useIconAndColor";
+
+export default {
+  props: {
+    character: {
+      type: Object,
+      required: true,
     },
+    showEditButton: {
+      type: Boolean,
+      default: true,
     },
-  };
-  </script>
-  
+  },
+  setup() {
+    const { getSkillIcon, getPropertyColor, getTextColor } = useIconAndColor();
+
+    return {
+      getSkillIcon,
+      getPropertyColor,
+      getTextColor,
+    };
+  },
+};
+</script>
