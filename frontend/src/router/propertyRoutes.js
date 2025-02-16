87f8a81b31f743d59729d726f8531ae18d40a6a1
BeomJo-uos <81895680+BeomJo-uos@users.noreply.github.com>
2025-02-16 16:59:47 +0900

    front 저장소 분리

--- frontend/src/router/propertyRoutes.js
+++
@@ -1,7 +0,0 @@
-import PropertyListView from '@/views/property/PropertyListView.vue';
-import AddProperty from '@/views/property/AddProperty.vue';
-
-export default [
-    { path: "/property-list", name: "PropertyList", component: PropertyListView },
-    { path: "/property/add", component: AddProperty},
-]
